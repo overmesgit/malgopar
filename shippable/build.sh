bfbc0921395d17d2a04f2c8e6beb3cc75030556a
overmes <overmes@gmail.com>
2016-10-22 00:07:50 +0300

    shippable

--- shippable/build.sh
+++ shippable/build.sh
@@ -2,4 +2,5 @@ set +e
 export GOPATH=$GOPATH:/root/src/github.com/overmesgit/malgopar/
 go test -v `ls /root/src/github.com/overmesgit/malgopar/src/ -I main`
 go build -v /root/src/github.com/overmesgit/malgopar/src/main/worker.go
+go build -v /root/src/github.com/overmesgit/malgopar/src/main/group.go
 set -e
--- shippable/success.sh
+++ shippable/success.sh
@@ -1,2 +1,3 @@
 ls -l /tmp/ssh
 scp -o StrictHostKeyChecking=no -i /tmp/ssh/lite worker root@linime.animesos.net:/root/
+scp -o StrictHostKeyChecking=no -i /tmp/ssh/lite group root@linime.animesos.net:/root/
