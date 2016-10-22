bca89cfbb465f1632bdec10c380c7256bc579629
overmes <overmes@gmail.com>
2016-10-22 13:30:48 +0300

    shippable

--- shippable/success.sh
+++ shippable/success.sh
@@ -1,3 +1,3 @@
-scp -o StrictHostKeyChecking=no -i /tmp/ssh/lite worker root@linime.animesos.net:/root/
-scp -o StrictHostKeyChecking=no -i /tmp/ssh/lite group root@linime.animesos.net:/root/
-scp -o StrictHostKeyChecking=no -i /tmp/ssh/lite infinite_worker root@linime.animesos.net:/root/
+scp -o StrictHostKeyChecking=no -i /tmp/ssh/lite worker root@linime.animesos.net:/root/build/
+scp -o StrictHostKeyChecking=no -i /tmp/ssh/lite group root@linime.animesos.net:/root/build/
+scp -o StrictHostKeyChecking=no -i /tmp/ssh/lite infinite_worker root@linime.animesos.net:/root/build/
