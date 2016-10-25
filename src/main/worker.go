93ea6ec7aa2f0e4344ffa6dd2726992539477b77
overmes <overmes@gmail.com>
2016-10-25 23:58:15 +0300

    update only existed titles

--- src/main/worker.go
+++ src/main/worker.go
@@ -7,18 +7,10 @@ import (
 )
 
 func main() {
-	from, err := strconv.Atoi(os.Args[1])
+	workers, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
-	to, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		panic(err)
-	}
-	workers, err := strconv.Atoi(os.Args[3])
-	if err != nil {
-		panic(err)
-	}
-	postgres := os.Args[4]
-	malspider.StartSpider(from, to, false, workers, postgres)
+	postgres := os.Args[2]
+	malspider.StartSpider(false, workers, postgres)
 }
